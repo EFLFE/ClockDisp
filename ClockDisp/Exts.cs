914cd4ed2352f4ce7a6beb39eb399309504c8375
EFLFE <0eflfe0@gmail.com>
2018-02-11 20:53:01 +0300

    ready

---
+++ ClockDisp/Exts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace ClockDisp
+{
+    internal static class Exts
+    {
+        public static void ToggleOpacity(this UIElement uIElement)
+        {
+            uIElement.Opacity = uIElement.Opacity >= 1.0 ? 0.0 : 1.0;
+        }
+
+        public static void Hide(this UIElement uIElement)
+        {
+            // скрыт, но мы всё ещё можем по нему кликать
+            uIElement.Opacity = 0.0;
+        }
+
+        public static void Show(this UIElement uIElement)
+        {
+            uIElement.Opacity = 1.0;
+        }
+    }
+}
--- ClockDisp/MainWindow.xaml.cs
+++ ClockDisp/MainWindow.xaml.cs
@@ -21,102 +21,66 @@ namespace ClockDisp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private sealed class LedData
+        // Red index
+        private enum DischargeEnum
         {
-            public readonly bool D0;
-            public readonly bool D1;
-            public readonly bool D2;
-            public readonly bool D3;
-            public readonly bool D4;
-            public readonly bool D5;
-            public readonly bool D6;
-
-            public LedData(bool d0, bool d1, bool d2, bool d3, bool d4, bool d5, bool d6)
-            {
-                D0 = d0;
-                D1 = d1;
-                D2 = d2;
-                D3 = d3;
-                D4 = d4;
-                D5 = d5;
-                D6 = d6;
-            }
-
-            //                        0 23 5 78 10`
-            /// <summary> sample: 1: '0-01-0-01-0' </summary>
-            public LedData(string data)
-            {
-                if (data.Length != "0-01-0-01-0".Length)
-                    throw new Exception("LedData bad format. Example: '0-01-0-01-0'");
-
-                D0 = data[0] == '1';
-                D1 = data[2] == '1';
-                D2 = data[3] == '1';
-                D3 = data[5] == '1';
-                D4 = data[7] == '1';
-                D5 = data[8] == '1';
-                D6 = data[10] == '1';
-            }
-        }
-
-        private enum LedIndexEnum
-        {
-            Led_0 = 0,
             Led_1 = 1,
             Led_2 = 2,
             Led_3 = 3,
+            Led_4 = 4,
             // Led_All
         }
 
-        private enum LedSetItemEnum
-        {
-            Disable,
-            None,
-            Enable,
-        }
-
+        // ссылки на изображения для включения/выкл отдельных палочек
         // key for d_1_4 (led 1, item 4) = '1-4'
         private readonly Dictionary<string, Image> imageLedIndexSet;
+
+        // бинарные данные для отрисовки символов
         private readonly Dictionary<char, LedData> ledDataSet;
 
-        // for easy code reading
+        // для лёгкого чтения кода
         private const bool
             ON = true,
             OFF = false;
 
+        // конструктор
         public MainWindow()
         {
             InitializeComponent();
 
+            // мигание точек
             ThreadPool.QueueUserWorkItem(new WaitCallback((_) =>
             {
-                while (true)
+                while (true) // так как это пул поток, он завершится сам по закрытии программы
                 {
+                    // находясь в отдельном потоке
+                    // в WPF изменения элементов из WPF формы нужно выполнять в методе Dispatcher.Invoke
+
                     Thread.Sleep(1000);
-                    Dispatcher.Invoke(() => { spl.Visibility = Visibility.Hidden; });
+                    Dispatcher.Invoke(() => { spl.Opacity = 0.0; });
                     Thread.Sleep(1000);
-                    Dispatcher.Invoke(() => { spl.Visibility = Visibility.Visible; });
+                    Dispatcher.Invoke(() => { spl.Opacity = 1.0; });
                 }
             }));
 
             // init refs for led index
-            imageLedIndexSet = new Dictionary<string, Image>(28)
+            imageLedIndexSet = new Dictionary<string, Image>
             {
-                { "0-0", d_0_0 },
-                { "0-1", d_0_1 },
-                { "0-2", d_0_2 },
-                { "0-3", d_0_3 },
-                { "0-4", d_0_4 },
-                { "0-5", d_0_5 },
-                { "0-6", d_0_6 },
+                { "4-0", d_0_0 },
+                { "4-1", d_0_1 },
+                { "4-2", d_0_2 },
+                { "4-3", d_0_3 },
+                { "4-4", d_0_4 },
+                { "4-5", d_0_5 },
+                { "4-6", d_0_6 },
 
-                { "1-0", d_1_0 },
-                { "1-1", d_1_1 },
-                { "1-2", d_1_2 },
-                { "1-3", d_1_3 },
-                { "1-4", d_1_4 },
-                { "1-5", d_1_5 },
-                { "1-6", d_1_6 },
+                { "3-0", d_1_0 },
+                { "3-1", d_1_1 },
+                { "3-2", d_1_2 },
+                { "3-3", d_1_3 },
+                { "3-4", d_1_4 },
+                { "3-5", d_1_5 },
+                { "3-6", d_1_6 },
 
                 { "2-0", d_2_0 },
                 { "2-1", d_2_1 },
@@ -126,17 +90,17 @@ namespace ClockDisp
                 { "2-5", d_2_5 },
                 { "2-6", d_2_6 },
 
-                { "3-0", d_3_0 },
-                { "3-1", d_3_1 },
-                { "3-2", d_3_2 },
-                { "3-3", d_3_3 },
-                { "3-4", d_3_4 },
-                { "3-5", d_3_5 },
-                { "3-6", d_3_6 },
+                { "1-0", d_3_0 },
+                { "1-1", d_3_1 },
+                { "1-2", d_3_2 },
+                { "1-3", d_3_3 },
+                { "1-4", d_3_4 },
+                { "1-5", d_3_5 },
+                { "1-6", d_3_6 },
             };
 
-            // init led chars data
-            ledDataSet = new Dictionary<char, LedData>()
+            // создание бинарных данных (буквы только нижнего регистра)
+            ledDataSet = new Dictionary<char, LedData>
             {
                 { '0', new LedData("1-11-0-11-1") },
                 { '1', new LedData("0-01-0-01-0") },
@@ -149,8 +113,21 @@ namespace ClockDisp
                 { '8', new LedData("1-11-1-11-1") },
                 { '9', new LedData("1-11-1-01-1") },
 
-                //{ '?', new LedData("") },
+                { '-', new LedData("0-00-1-00-0") },
             };
+
+            // отключить все кнопки
+            week_day.Hide();
+            monday.Hide();
+            tuesday.Hide();
+            wendesday.Hide();
+            thursday.Hide();
+            friday.Hide();
+            saturday.Hide();
+            sunday.Hide();
+            bell.Hide();
+            program.Hide();
+            timer.Hide();
 
             PrintTime();
         }
@@ -161,36 +138,37 @@ namespace ClockDisp
             string hour = DateTime.Now.Hour.ToString();
             if (hour.Length > 1)
             {
-                Print(LedIndexEnum.Led_0, hour[0]);
-                Print(LedIndexEnum.Led_1, hour[1]);
+                Print(DischargeEnum.Led_4, hour[0]);
+                Print(DischargeEnum.Led_3, hour[1]);
             }
             else
             {
-
-                Print(LedIndexEnum.Led_0, '0');
-                Print(LedIndexEnum.Led_1, hour[0]);
+                Print(DischargeEnum.Led_4, '0');
+                Print(DischargeEnum.Led_3, hour[0]);
             }
 
             string min = DateTime.Now.Minute.ToString();
             if (min.Length > 1)
             {
-                Print(LedIndexEnum.Led_2, min[0]);
-                Print(LedIndexEnum.Led_3, min[1]);
+                Print(DischargeEnum.Led_2, min[0]);
+                Print(DischargeEnum.Led_1, min[1]);
             }
             else
             {
-
-                Print(LedIndexEnum.Led_2, '0');
-                Print(LedIndexEnum.Led_3, min[0]);
+                Print(DischargeEnum.Led_2, '0');
+                Print(DischargeEnum.Led_1, min[0]);
             }
         }
 
         /// <summary> Напечатать символ. </summary>
-        /// <param name="ledIndex">Red index.</param>
+        /// <param name="ledIndex">Разряд.</param>
         /// <param name="symbol">Symbol.</param>
-        private void Print(LedIndexEnum ledIndex, char symbol)
+        private void Print(DischargeEnum ledIndex, char symbol)
         {
             int lednum = (int)ledIndex;
+
+            if (char.IsLetter(symbol))
+                symbol = char.ToLower(symbol);
 
             if (ledDataSet.TryGetValue(symbol, out LedData ld))
             {
@@ -211,16 +189,13 @@ namespace ClockDisp
         /// <param name="data">Led data from 'ledDataSet'.</param>
         private void Print(int ledIndex, LedData ledData)
         {
-            if (ledIndex < -1 || ledIndex > 3)
-                throw new IndexOutOfRangeException("Print: arg 'ledIndex' out of range (only from 0 to 3).");
-
-            imageLedIndexSet[ledIndex + "-0"].Visibility = ledData.D0 == true ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-1"].Visibility = ledData.D1 == true ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-2"].Visibility = ledData.D2 == true ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-3"].Visibility = ledData.D3 == true ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-4"].Visibility = ledData.D4 == true ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-5"].Visibility = ledData.D5 == true ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-6"].Visibility = ledData.D6 == true ? Visibility.Visible : Visibility.Hidden;
+            imageLedIndexSet[ledIndex + "-0"].Opacity = ledData.A ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-1"].Opacity = ledData.B ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-2"].Opacity = ledData.C ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-3"].Opacity = ledData.D ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-4"].Opacity = ledData.E ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-5"].Opacity = ledData.F ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-6"].Opacity = ledData.G ? 1.0 : 0.0;
         }
 
         /// <summary> Напечатать символ. </summary>
@@ -228,9 +203,6 @@ namespace ClockDisp
         /// <param name="data">Strign index set (0-01-0-01-0).</param>
         private void Print(int ledIndex, string data)
         {
-            if (ledIndex < -1 || ledIndex > 3)
-                throw new IndexOutOfRangeException("Print: arg 'ledIndex' out of range (only from 0 to 3).");
-
             imageLedIndexSet[ledIndex + "-0"].Visibility = data[0] == '0' ? Visibility.Visible : Visibility.Hidden;
             imageLedIndexSet[ledIndex + "-1"].Visibility = data[2] == '0' ? Visibility.Visible : Visibility.Hidden;
             imageLedIndexSet[ledIndex + "-2"].Visibility = data[3] == '0' ? Visibility.Visible : Visibility.Hidden;
@@ -242,25 +214,88 @@ namespace ClockDisp
 
         /// <summary> Напечатать символ. </summary>
         /// <param name="ledIndex">Red index.</param>
-        /// <param name="d0">Magenta index 0.</param>
+        /// <param name="a">Magenta index 0.</param>
         /// <param name="d1">Magenta index 1.</param>
         /// <param name="d2">Magenta index 2.</param>
         /// <param name="d3">Magenta index 3.</param>
         /// <param name="d4">Magenta index 4.</param>
         /// <param name="d5">Magenta index 5.</param>
         /// <param name="d6">Magenta index 6.</param>
-        private void Print(int ledIndex, bool d0, bool d1, bool d2, bool d3, bool d4, bool d5, bool d6)
+        private void Print(int ledIndex, bool a, bool b, bool c, bool d, bool e, bool f, bool g)
         {
-            if (ledIndex < -1 || ledIndex > 3)
-                throw new IndexOutOfRangeException("Print: arg 'ledIndex' out of range (only from 0 to 3).");
+            imageLedIndexSet[ledIndex + "-0"].Opacity = a ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-1"].Opacity = b ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-2"].Opacity = c ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-3"].Opacity = d ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-4"].Opacity = e ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-5"].Opacity = f ? 1.0 : 0.0;
+            imageLedIndexSet[ledIndex + "-6"].Opacity = g ? 1.0 : 0.0;
+        }
 
-            imageLedIndexSet[ledIndex + "-0"].Visibility = d0 == ON ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-1"].Visibility = d1 == ON ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-2"].Visibility = d2 == ON ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-3"].Visibility = d3 == ON ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-4"].Visibility = d4 == ON ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-5"].Visibility = d5 == ON ? Visibility.Visible : Visibility.Hidden;
-            imageLedIndexSet[ledIndex + "-6"].Visibility = d6 == ON ? Visibility.Visible : Visibility.Hidden;
+        // [БУД]
+        private void OnWeekDayClick(object sender, MouseButtonEventArgs e)
+        {
+            week_day.ToggleOpacity();
+        }
+
+        // [ПН]
+        private void OnMondayClick(object sender, MouseButtonEventArgs e)
+        {
+            monday.ToggleOpacity();
+        }
+
+        // [ВТ]
+        private void OnTuesdayClick(object sender, MouseButtonEventArgs e)
+        {
+            tuesday.ToggleOpacity();
+        }
+
+        // [СР]
+        private void OnWendesdayClick(object sender, MouseButtonEventArgs e)
+        {
+            wendesday.ToggleOpacity();
+        }
+
+        // [ЧТ]
+        private void OnThursdayClick(object sender, MouseButtonEventArgs e)
+        {
+            thursday.ToggleOpacity();
+        }
+
+        // [ПТ]
+        private void OnFridayClick(object sender, MouseButtonEventArgs e)
+        {
+            friday.ToggleOpacity();
+        }
+
+        // [СБ]
+        private void OnSaturdayClick(object sender, MouseButtonEventArgs e)
+        {
+            saturday.ToggleOpacity();
+        }
+
+        // [ВС]
+        private void OnSundayClick(object sender, MouseButtonEventArgs e)
+        {
+            sunday.ToggleOpacity();
+        }
+
+        // [колокольчик]
+        private void OnBellClick(object sender, MouseButtonEventArgs e)
+        {
+            bell.ToggleOpacity();
+        }
+
+        // [ПРГ]
+        private void OnProgramClick(object sender, MouseButtonEventArgs e)
+        {
+            program.ToggleOpacity();
+        }
+
+        // [таймер]
+        private void OnTimerClick(object sender, MouseButtonEventArgs e)
+        {
+            timer.ToggleOpacity();
         }
 
     }
